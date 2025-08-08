1cdb3dd2d428c92d690fb51cbffaba03c8a7ef3a
rhaversen <rhaversen@gmail.com>
2025-08-08 18:09:04 +0200

    Update renderAllPages.bat to define pages for curl requests

--- renderAllPages.bat
+++ renderAllPages.bat
@@ -2,7 +2,7 @@ setlocal enabledelayedexpansion
 
 start /B curl http://localhost:3000/ > NUL 2>&1
 
-set pages=TODO
+set pages=404 confirm login reset-password signup
 
 for %%p in (%pages%) do (
     start /B curl http://localhost:3000/%%p > NUL 2>&1
