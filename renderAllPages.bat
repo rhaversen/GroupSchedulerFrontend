29c40553f125c0a9f79d41222903798b5eca6835
rhaversen <rhaversen@gmail.com>
2025-08-09 15:52:40 +0200

    Update renderAllPages script to include additional event pages for improved navigation

--- renderAllPages.bat
+++ renderAllPages.bat
@@ -2,7 +2,7 @@ setlocal enabledelayedexpansion
 
 start /B curl http://localhost:3000/ > NUL 2>&1
 
-set pages=404 confirm dashboard events events/id events/new login profile reset-password signup
+set pages=404 confirm dashboard events events/browse events/my-events events/participating login people people/id profile reset-password service signup
 
 for %%p in (%pages%) do (
     start /B curl http://localhost:3000/%%p > NUL 2>&1
