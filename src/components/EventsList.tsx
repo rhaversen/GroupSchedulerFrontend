3f5eef2f21240de8f8c9b8eb042fa03cebf76328
rhaversen <rhaversen@gmail.com>
2025-08-10 14:40:38 +0200

    Reduce loading skeletons from 6 to 3 in EventsList component for improved performance

--- src/components/EventsList.tsx
+++ src/components/EventsList.tsx
@@ -24,7 +24,7 @@ export default function EventsList ({
 	if (loading) {
 		return (
 			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-				{[...Array(6)].map((_, i) => (
+				{[...Array(3)].map((_, i) => (
 					<div key={i} className="animate-pulse">
 						<div className="h-48 bg-gray-200 rounded-lg"></div>
 					</div>
